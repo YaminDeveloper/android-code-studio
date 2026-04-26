b6ef04527a6f728846697efe570f44196c73ae43
YAMIN AHMED <91419622+YaminDeveloper@users.noreply.github.com>
2026-04-26 05:45:10 +0300

    Update build.gradle.kts

--- composite-builds/build-deps/java-compiler/build.gradle.kts
+++ composite-builds/build-deps/java-compiler/build.gradle.kts
@@ -23,3 +23,17 @@ java {
   sourceCompatibility = JavaVersion.VERSION_1_8
   targetCompatibility = JavaVersion.VERSION_1_8
 }
+
+sourceSets {
+  main {
+    java {
+      exclude 'openjdk/tools/sjavac/comp/CompilationService.java'
+      exclude 'openjdk/tools/sjavac/comp/SjavacImpl.java'
+      exclude 'openjdk/tools/sjavac/comp/PooledSjavac.java'
+      exclude 'openjdk/tools/sjavac/client/ClientMain.java'
+      exclude 'openjdk/tools/sjavac/client/SjavacClient.java'
+      exclude 'openjdk/tools/sjavac/CompileJavaPackages.java'
+      exclude 'openjdk/tools/sjavac/Main.java'
+    }
+  }
+}
